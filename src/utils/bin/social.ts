fa1679e451f9c3425527126e76324ca9cdb2a2b6
Robert Weiner <robertw818@yahoo.com>
2023-05-24 22:28:48 -0500

    feat(config): theme and socials

--- src/utils/bin/social.ts
+++ src/utils/bin/social.ts
@@ -1,11 +1,5 @@
 import config from '../../../config.json';
 
-export const instagram = async (args: string[]): Promise<string> => {
-  window.open(`https://www.instagram.com/${config.social.instagram}/`);
-
-  return 'Opening instagram...';
-};
-
 export const github = async (args: string[]): Promise<string> => {
   window.open(`https://github.com/${config.social.github}/`);
 
